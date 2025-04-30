9a2b6aa1b3127cd52e8043352d1cdc541ec78235
tarun_kumar_tripathi <tripathitarun1620000@gmail.com>
2025-04-30 14:58:13 +0530

    Refactor: update file structure and add components

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./Search.module.css";
 import { ReactComponent as SearchIcon } from "../../assets/search-icon.svg";
-import useAutocomplete from "@mui/base/useAutocomplete";
+import { useAutocomplete } from '@mui/material';
 import { styled } from "@mui/system";
 import { truncate } from "../../helpers/helpers";
 import { useNavigate } from "react-router-dom";
 import { Tooltip } from "@mui/material";
+
+
 
 const Listbox = styled("ul")(({ theme }) => ({
   width: "100%",
